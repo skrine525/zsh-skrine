3d662a1f85b0d37f14c78a54f072179e3e6ae7b2
Skrine <tsaregorodtsev03@mail.ru>
2025-01-12 07:13:06 +1000

    [WIP]

--- install.sh
+++ install.sh
@@ -3,16 +3,23 @@
 REPO_URL="https://github.com/skrine525/zsh-skrine"
 DEST_DIR="$HOME/.zsh-skrine"
 
-# Проверяем, существует ли папка
 if [ -d "$DEST_DIR" ]; then
     echo "Updating..."
     cd "$DEST_DIR" || exit 1
-    git fetch origin
-    git rebase origin/dev
+    git fetch origin > /dev/null 2>&1
+    git rebase origin/dev > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Git rebase failed. Exiting..."
+        exit 1
+    fi
 else
     echo "Clonning..."
-    git clone "$REPO_URL" "$DEST_DIR"
-    git checkout dev
+    git clone "$REPO_URL" "$DEST_DIR" > /dev/null 2>&1
+    git checkout dev > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "Git clone failed. Exiting..."
+        exit 1
+    fi
 fi
 
 ln -sf "$HOME/.zsh-skrine/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
