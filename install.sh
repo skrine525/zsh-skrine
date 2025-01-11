6deb20d14332f3be4c235c9c5dcff1a9878d5e3a
Skrine <tsaregorodtsev03@mail.ru>
2025-01-12 06:59:09 +1000

    [WIP]

--- install.sh
+++ install.sh
@@ -15,5 +15,5 @@ else
     git checkout dev
 fi
 
-ln -s "$HOME/.zsh-skrine/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
+ln -sf "$HOME/.zsh-skrine/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
 zsh
