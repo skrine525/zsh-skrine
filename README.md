8df1b043794a7790c4ab6037f0eabe16d59a22b3
skrine525 <43129775+skrine525@users.noreply.github.com>
2025-01-12 19:38:10 +1000

    [FEAT] Provide early access (#1)

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# skrine-zsh-theme
+# skrine-zsh
+
+## Требования
+
+- **Zsh** — необходим для работы конфигураций.
+
+## Установка
+
+1. Установите `oh-my-zsh` (если он еще не установлен):
+
+    ```bash
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    ```
+
+2. Установите `skrine-zsh`:
+
+    ```bash
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/skrine525/zsh-skrine/refs/heads/main//install.sh)"
+    ```
+
+3. После установки добавьте строку для использования темы `skrine-theme` в файл конфигурации `.zshrc`:
+
+    ```bash
+    ZSH_THEME="skrine-theme"
+    ```
+
+## Как обновить
+
+Для обновления выполните команду:
+
+```bash
+update-theme
+```
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+
+REPO_URL="https://github.com/skrine525/zsh-skrine"
+DEST_DIR="$HOME/.zsh/zsh-skrine"
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is not installed. Please install it first."
+    exit 1
+fi
+
+if [ ! -d "$HOME/.zsh" ]; then
+    echo "Creating .zsh directory..."
+    mkdir -p "$HOME/.zsh"
+fi
+
+if [ -d "$DEST_DIR" ]; then
+    echo "Updating..."
+
+    if ! cd "$DEST_DIR"; then
+        echo "Failed to change directory to $DEST_DIR. Exiting..."
+        exit 1
+    fi
+
+    git fetch origin > /dev/null 2>&1
+    git reset --hard origin/main > /dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo "Updating failed. Exiting..."
+        exit 1
+    fi
+
+    ln -sf "$DEST_DIR/theme/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
+    echo "Success. Restart your zsh."
+else
+    echo "Installing..."
+
+    git clone "$REPO_URL" "$DEST_DIR" > /dev/null 2>&1
+
+    if [ $? -ne 0 ]; then
+        echo "Installing failed. Exiting..."
+        exit 1
+    fi
+
+    ln -sf "$DEST_DIR/theme/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
+    echo "Success."
+fi
