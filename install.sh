4d4631f37c0bde901595f64e121978a90f09a7d6
Skrine <tsaregorodtsev03@mail.ru>
2025-01-12 19:10:11 +1000

    [WIP]

--- install.sh
+++ install.sh
@@ -15,29 +15,29 @@ if [ ! -d "$HOME/.zsh" ]; then
 fi
 
 if [ -d "$DEST_DIR" ]; then
-    echo "Updating..."
+    echo "Installing..."
 
     cd "$DEST_DIR" || exit 1
     git fetch origin > /dev/null 2>&1
     git reset --hard origin/dev > /dev/null 2>&1
 
     if [ $? -ne 0 ]; then
-        echo "Git rebase failed. Exiting..."
+        echo "Installing failed. Exiting..."
         exit 1
     fi
 
-    ln -sf "$DEST_DIR/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
+    ln -sf "$DEST_DIR/theme/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
     echo "Success. Restart your zsh."
 else
-    echo "Cloning..."
+    echo "Updating..."
 
     git clone "$REPO_URL" "$DEST_DIR" > /dev/null 2>&1
 
     if [ $? -ne 0 ]; then
-        echo "Git clone failed. Exiting..."
+        echo "Updating failed. Exiting..."
         exit 1
     fi
 
-    ln -sf "$DEST_DIR/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
+    ln -sf "$DEST_DIR/theme/skrine-theme.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/skrine-theme.zsh-theme"
     echo "Success."
 fi
